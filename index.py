6ffea28f3a26b5dcf3ba577400e3418fbe6c871f
eduardocersilva772-hub <eduardocersilva772@gmail.com>
2026-09-01 23:28:27 -0600

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+# CERDA SILVA EDUARDO 
+# habit_tracker.py
+# Avance inicial del proyecto Habit Tracker
+
+def mostrar_menu():
+    print("=== HABIT TRACKER ===")
+    print("1. Ver hábitos")
+    print("2. Agregar hábito")
+    print("3. Marcar hábito")
+    print("4. Salir")
+
+if __name__ == "__main__":
+    mostrar_menu()
